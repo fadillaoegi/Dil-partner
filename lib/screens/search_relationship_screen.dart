6d7302a1f8505ee84a70a8f69e19e57a3efe3c70
fadillaoegi <fadilfdl2710@gmail.com>
2023-09-28 08:58:49 +0700

    Update

--- lib/screens/search_relationship_screen.dart
+++ lib/screens/search_relationship_screen.dart
@@ -1,7 +1,7 @@
 import 'package:dilpartner/styles/asset_manager.dart';
-import 'package:dilpartner/styles/colors.dart';
-import 'package:dilpartner/styles/fonts.dart';
 import 'package:dilpartner/widgets/button_circle.dart';
+import 'package:dilpartner/widgets/header_main_widget.dart';
+import 'package:dilpartner/widgets/swipe_people_widget.dart';
 import 'package:flutter/material.dart';
 
 class SearchRelationshipScreen extends StatefulWidget {
@@ -17,52 +17,20 @@ class _SearchRelationshipScreenState extends State<SearchRelationshipScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 24.0),
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        margin: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // NOTE: HEADER
-            Container(
-              margin: const EdgeInsets.only(top: 40.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CircleAvatar(
-                      backgroundImage: AssetImage(
-                          "${DilAssetManager.asset}/user_image.png")),
-                  Text(
-                    "DILPARTNER",
-                    style: logo400.copyWith(fontSize: 28.0),
-                  ),
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.notifications_none_outlined))
-                ],
-              ),
-            ),
+            const HeaderMainWidget(),
             const SizedBox(
-              height: 50.0,
+              height: 40.0,
             ),
 
             // NOTE: SLIDE CONTENT
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(10.0),
-              height: 540.0,
-              decoration: BoxDecoration(
-                  color: DilPartnerColor.cardColor,
-                  borderRadius: BorderRadius.circular(70.0)),
-              child: Container(
-                height: 518.0,
-                width: 3255.0,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(60.0),
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(
-                            "${DilAssetManager.asset}/people_love3_image.png"))),
-              ),
-            ),
+            const SwipePeopleWidget(),
 
             const SizedBox(
               height: 50.0,
---
+++ lib/widgets/swipe_people_widget.dart
@@ -0,0 +1,74 @@
+import 'package:dilpartner/styles/asset_manager.dart';
+import 'package:dilpartner/styles/colors.dart';
+import 'package:dilpartner/styles/fonts.dart';
+import 'package:flutter/material.dart';
+
+class SwipePeopleWidget extends StatelessWidget {
+  const SwipePeopleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        // NOTE: CONTAINER PROFILE
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              color: DilPartnerColor.cardColor,
+              borderRadius: BorderRadius.circular(70.0)),
+          child: Container(
+            height: 518.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60.0),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                        "${DilAssetManager.asset}/people_love3_image.png"))),
+          ),
+        ),
+
+        // NOTE: INFORMASI USER
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 21.0, vertical: 20.0),
+          width: double.infinity,
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(33.0),
+              image: DecorationImage(
+                  image: AssetImage(
+                      "${DilAssetManager.asset}/glass_card_image.png"))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // NOTE: TEXT
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Nabila",
+                    style: white500.copyWith(fontSize: 24.0),
+                  ),
+                  Text(
+                    "21, Ustadza",
+                    style: white300.copyWith(fontSize: 16.0),
+                  ),
+                ],
+              ),
+
+              // NOTE: ICON
+              Image.asset(
+                "${DilAssetManager.asset}/icon_profile.png",
+                width: 50.0,
+                height: 50.0,
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
